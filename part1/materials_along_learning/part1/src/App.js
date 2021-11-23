768f2e38b0e4b6a0e99a3fce5ff13afa7308650e
JabSysEmb <abduljabbar.sha97@gmail.com>
2021-11-23 18:12:24 +0300

    exercise 1.1

--- part1/materials_along_learning/part1/src/App.js
+++ part1/materials_along_learning/part1/src/App.js
@@ -1,21 +1,59 @@
 import React from 'react';
-import './App.css';
+//  import './App.css';
 
-const Ahmed = (props) => {
+function Part(props) {
   return(
-    <p>My name is {props.name}!</p>
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+function Content(props) {
+  return(
+    <>
+    <Part part={props.part1} exercises={props.exercises1}/>
+    <Part part={props.part2} exercises={props.exercises2}/>
+    <Part part={props.part3} exercises={props.exercises3}/>
+    </>
   )
 }
 
+const Header  = (props) => <h1>{props.course}</h1>
+const Total   = (props) => <p>Number of exercises {props.exercises}</p>
+
 function App() {
+  const course = 'Half Stack application development'
+  let exercises = {
+    content1 : {
+      part      : 'Fundamentals of React',
+      exercises : 10,
+    },
+    content2 : {
+      part      : 'Using props to pass data',
+      exercises : 7
+    },
+    content3 : {
+      part      : 'State of a component',
+      exercises : 14
+    }
+  }
+  
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
-    <div>
-      <div className="App-header">
-        <p>Hello, world! we are in a shity world.</p>
-        <Ahmed name="rami" date={Date()} wieght="75Kg"/>
-      </div>
+    <div className="App-header">
+      < Header course={course} />
+      < Content part1={part1} exercises1={exercises1} 
+                part2={part2} exercises2={exercises2}
+                part3={part3} exercises3={exercises3} />
+      < Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
-  );
+  )
 }
 
 export default App;
