58832cdbfec2174afabcf78286c78674f1615084
JabSysEmb <abduljabbar.sha97@gmail.com>
2021-11-25 00:54:14 +0300

    section B of the Part1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,27 +6,27 @@
 */
 
 import React from 'react';
-//  import './App.css';
+import './App.css';
 
 
-const Part = (props) => <p> {props.part.part} {props.part.exercises} </p>
+const Part = (props) => <p > {props.part.part} {props.part.exercises} </p>
 
-
-const Content = (props) => <p> {props.course.parts.map((item) => (<Part part={item} />))} </p>
+const Content = (props) => <> {props.course.parts.map((item) => (<Part part={item} />))} </>
 
 const Total = (props) => <p> {props.course.msg} {props.course.parts.reduce((result,item) => (item.exercises + result),0)} </p>
 
-
 const Header  = (props) => <h1>{props.course.name}</h1>
 
-const App = () => {
+const Counter = (props) => <h2> counter : {props.counter}</h2>
+
+const App = (props) => {
   const course = {
     name  : 'Half Stack application development', 
     msg   : 'Number of exercises',
     parts : [
       {
         part      : 'Fundamentals of React',
-        exercises : 10
+        exercises : 11
       },
       {
         part      : 'Using props to pass data',
@@ -36,11 +36,13 @@ const App = () => {
         part      : 'State of a component',
         exercises : 14
       }
-    ]
+    ],
+    refresh_num   : 0
   };
-
+  
   return (
     <div className="App-header">
+      < Counter counter={props.counter} />
       < Header  course={course} />
       < Content course={course} />
       < Total   course={course} />
@@ -48,4 +50,11 @@ const App = () => {
   )
 }
 
-export default App;
+const increse_refresh_num = () => {this.course.refresh_num+=1}
+
+const exporter ={
+  App,
+  increse_refresh_num
+}
+
+export default exporter;
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import exporter from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <App />,
+const App = exporter.App;
+
+let counter=1;
+
+const refresh = () => ReactDOM.render(
+  <App counter={counter}/>,
   document.getElementById('root'),
 );
-
+counter += 3
+refresh()
+counter += 1
+refresh()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
