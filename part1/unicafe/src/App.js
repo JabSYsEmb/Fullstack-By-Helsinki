2ce5725e21fc54faea4c079f83fe734ddc92df9d
JabSysEmb <abduljabbar.sha97@gmail.com>
2021-12-02 11:55:53 +0300

    refeactoring App.js

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,29 +1,49 @@
 import { useState } from 'react';
 import './App.css';
 
-const Statistics  = ({data})    =>  isSumZero(data) ? 
-                                    "No feedbacks given" : 
-                                    Object.entries(data)
-                                            .map((item,index) => < StatisticsLine value={item[1]} msg={item[0]} className={item[0]} />)
-                                        
-const StatisticsLine  = ({value,msg,className})    => <p className={className}>{msg} : {value}</p>
+const Statistics  = ({data}) =>  {
+  return isSumZero(data) ? 
+    dataNotFound() : getStatistics(data);
+}    
 
-const Header          = ({header,className})       => <h1 className={className}>{header}</h1>
-const Button          = ({onClick,text,className}) => <button onClick={onClick} className={className}>{text}</button>
+const getStatistics            = (data) => (
+  <>
+  {Object.entries(data).map((item) => 
+        < StatisticsLine key={item[0]} value={item[1]} msg={item[0]} />)}
+  {Object.entries(getRemainingStatistics(data)).map((item) => 
+        < StatisticsLine key={item[0]} value={item[1]} msg={item[0]} />)}
+  </>
+)
 
-const sumOfFeedbacks  = (feedbacks)                => Object.entries(feedbacks).reduce((result,item) => (result+item[1]),0);
-const fixFloat        = (float_num,step)           => float_num.toFixed(step)
-const isSumZero       = (feedbacks)                => (sumOfFeedbacks(feedbacks) === 0 )
+const getRemainingStatistics   = (data) => {
+  const metrics = {
+    sum     : sumOfFeedbacks(data),
+    average : findAverageOfFeedbacks(data),
+    postive : findPositiveRatioInFeedbacks(data)
+  }
+  return metrics;
+}
 
 const findAverageOfFeedbacks       = (feedbacks) => (
   isSumZero(feedbacks) ?
-  "No feedbacks given":fixFloat((feedbacks.good+feedbacks.bad)/sumOfFeedbacks(feedbacks),4)
-  )
+  dataNotFound() : fixFloat((feedbacks.good+feedbacks.bad)/sumOfFeedbacks(feedbacks),4)
+)
 
 const findPositiveRatioInFeedbacks = (feedbacks) => (
   isSumZero(feedbacks) ?
-  "No feedbacks given":fixFloat(feedbacks.good/sumOfFeedbacks(feedbacks),2) * 100 + " %"
-  )
+  dataNotFound() : fixFloat(feedbacks.good/sumOfFeedbacks(feedbacks),3) * 100 + " %"
+)
+
+const StatisticsLine  = ({value,msg})    => <p >{msg} : {value}</p>
+const dataNotFound    = ()               => <p>No feedbacks given</p>
+
+const sumOfFeedbacks  = (feedbacks)      => Object.entries(feedbacks).reduce((result,item) => (result+item[1]),0);
+const fixFloat        = (float_num,step) => float_num.toFixed(step)
+const isSumZero       = (feedbacks)      => (sumOfFeedbacks(feedbacks) === 0 )
+
+const Header          = ({header})       => <h1 >{header}</h1>
+const Button          = ({onClick,text}) => <button onClick={onClick} >{text}</button>
+
 
 function App() {
   const feedback_header   = "Give feedback";
@@ -38,20 +58,16 @@ function App() {
   const giveGood    = () => updateFeedbacks({...data, good    : data.good + 1   } )
   const giveNeutral = () => updateFeedbacks({...data, neutral : data.neutral + 1} )
   const giveBad     = () => updateFeedbacks({...data, bad     : data.bad + 1    } )
-
   // console.log(Object.entries(feedbacks).map(item => item[0]+ " : " +item[1]))
   return (
     <div className="App">
       <header className="hello-world">
-        < Header header={feedback_header}   className="main-color" />
+        < Header header={feedback_header}   />
         < Button onClick={giveGood}    text="Good"    className="good"    />
         < Button onClick={giveNeutral} text="Neutral" className="neutral" />
         < Button onClick={giveBad}     text="Bad"     className="bad"     />
-        < Header header={statistics_header} className="main-color" />
+        < Header header={statistics_header} />
         < Statistics data={data}/>
-        < StatisticsLine value={sumOfFeedbacks(data)} msg="All" className="main-color" />
-        < StatisticsLine value={findAverageOfFeedbacks(data)} msg="Average" className="main-color" />
-        < StatisticsLine value={findPositiveRatioInFeedbacks(data)} msg="Postive" className="main-color" />
       </header>
     </div>
   );
