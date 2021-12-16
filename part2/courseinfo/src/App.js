0c1abc082cded4618031127a52a007e3a4eddda3
JabSysEmb <abduljabbar.sha97@gmail.com>
2021-12-16 22:38:17 +0300

    xhr sync and async flag

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,18 +1,21 @@
+let data;
+
 const xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = () => {
   if (xhttp.readyState === 4 && xhttp.status === 200){
-    console.log("done")
-    JSON.parse(xhttp.responseText).map(
-      item => {
-        let elment = document.createElement('li')
-        elment.appendChild(document.createTextNode(item.content))
-        document.getElementsByClassName("div-content")[0].appendChild(elment)
-    })
+    data = JSON.parse(xhttp.responseText);
+
+    // JSON.parse(xhttp.responseText).map(
+    //   item => {
+    //     let elment = document.createElement('li')
+    //     elment.appendChild(document.createTextNode(item.content))
+    //     document.getElementsByClassName("div-content")[0].appendChild(elment)
+    // })
   }
 }
 
-xhttp.open('GET', 'http://localhost:3003/notes', true)
+xhttp.open('GET', 'http://localhost:3003/notes', false)
 xhttp.send()
 
 const App = () => {
@@ -20,7 +23,9 @@ const App = () => {
   return (
     <>
     <h1>Data</h1>
-    <ul className="div-content"></ul>
+    <ul className="div-content">
+      {data?.map(item => <p key={item.id}>{item.content}</p>)}
+    </ul>
     </>
     )
 }
