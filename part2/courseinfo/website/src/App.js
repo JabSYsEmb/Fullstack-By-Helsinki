f5ce47af08c33355c2bb532d1935fd9623c74b5d
JabSysEmb <abduljabbar.sha97@gmail.com>
2021-12-21 00:55:03 +0300

    the part2 has been containerized

--- part2/courseinfo/website/src/App.js
+++ part2/courseinfo/website/src/App.js
@@ -6,16 +6,16 @@ xhttp.onreadystatechange = () => {
   if (xhttp.readyState === 4 && xhttp.status === 200){
     data = JSON.parse(xhttp.responseText);
 
-    // JSON.parse(xhttp.responseText).map(
-    //   item => {
-    //     let elment = document.createElement('li')
-    //     elment.appendChild(document.createTextNode(item.content))
-    //     document.getElementsByClassName("div-content")[0].appendChild(elment)
-    // })
+    JSON.parse(xhttp.responseText).map(
+      item => {
+        let elment = document.createElement('li')
+        elment.appendChild(document.createTextNode(item.content))
+        document.getElementsByClassName("div-content")[0].appendChild(elment)
+    })
   }
 }
 
-xhttp.open('GET', 'http://localhost:3003/notes', false)
+xhttp.open('GET', 'http://localhost:3002/notes', true)
 xhttp.send()
 
 const App = () => {
@@ -24,7 +24,7 @@ const App = () => {
     <>
     <h1>Data</h1>
     <ul className="div-content">
-      {data?.map(item => <p key={item.id}>{item.content}</p>)}
+      {/* {data?.map(item => <p key={item.id}>{item.content}</p>)} */}
     </ul>
     </>
     )
