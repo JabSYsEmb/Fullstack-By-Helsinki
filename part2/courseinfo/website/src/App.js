696e06b96786456c38198438031d25ab5f7a7332
JabSysEmb <abduljabbar.sha97@gmail.com>
2021-12-22 12:22:24 +0300

    useEffect

--- part2/courseinfo/website/src/App.js
+++ part2/courseinfo/website/src/App.js
@@ -1,16 +1,33 @@
 import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
+let fireEffect = 0;
 
 const App = () => {
   const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState([])
+  
   useEffect(() => {
-    document.title = `You clicked ${count} times`
-  })
+    console.log('effect')
+    axios
+    .get('http://localhost:3002/notes')
+    .then((promises) => 
+    {
+      console.log('effect fullfilled')
+      setNotes(promises.data)
+    })
+  }, [fireEffect])
+
+  console.log('render',notes.length, 'notes');
 
   return (
     <>
       <p>You clicked {count} times no this button.</p>
+      <ul>
+        {notes.map(item => <li key={item.id}>{item.content}</li>)}
+      </ul>
       <button onClick={()=>setCount(count+1)}>click me</button>
+      {console.log('rendered')}
     </>
     )
 }
