5f6a23e44c95f2b691513becf14a35b77d80f9b0
JabSysEmb <abduljabbar.sha97@gmail.com>
2021-12-24 21:43:46 +0300

    courseinfo

--- part2/courseinfo/website/src/App.js
+++ part2/courseinfo/website/src/App.js
@@ -1,33 +1,50 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
+const jsonServerUrl = 'http://localhost:3002/notes'
+
 const App = () => {
-  const [count, setCount] = useState(0)
   const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('new note')
+  
+  const addNewNote = (event) => {
+    event.preventDefault()
+    const newDataInstance = {
+      id:         notes.length+1,
+      date:       new Date().toString(),
+      content:    newNote,
+      important:  Math.random() < 0.5
+    }
+    postDataToTheServer(jsonServerUrl,newDataInstance)
+  }
+
+  const postDataToTheServer = (serverUrl, newData) => {
+    axios.post(serverUrl,newData).then(
+      response=>
+        {
+          setNotes(notes.concat(response.data))
+          setNewNote('')
+        }
+      )
+  } 
   
   useEffect(() => {
-    console.log('effect')
-    axios
-    .get('http://localhost:3002/notes')
-    .then((promises) => 
-    {
-      console.log('effect fullfilled')
-      setNotes(promises.data)
-    })
+    axios.get(jsonServerUrl).then((promises) => {setNotes(promises.data)})
   }, [])
-
-  useEffect(() => {document.title = `${count} times clicked`})
   
-  console.log('render',notes.length, 'notes');
+  const newNoteChangeHandler = (event) => {
+    setNewNote(event.target.value)
+  }
 
   return (
     <>
-      <p>You clicked {count} times no this button.</p>
       <ul>
         {notes.map(item => <li key={item.id}>{item.content}</li>)}
       </ul>
-      <button onClick={()=>setCount(count+1)}>click me</button>
-      {console.log('rendered')}
+      <form >
+        <input value={newNote} onChange={newNoteChangeHandler}/>
+        <button type="submit" onClick={addNewNote}>add note</button>
+      </form>
     </>
     )
 }
