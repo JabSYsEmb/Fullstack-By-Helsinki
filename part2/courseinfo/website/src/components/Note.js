752e75b6b7099b20de220d8d7786ebfda4d9eeaa
JabSysEmb <abduljabbar.sha97@gmail.com>
2021-12-26 16:00:02 +0300

    end of part2 styling css

--- part2/courseinfo/website/src/components/Note.js
+++ part2/courseinfo/website/src/components/Note.js
@@ -4,18 +4,23 @@ import '../style/note.css'
 const NotesHtml = ({notes,toggleImportance}) => {
     return (
         <ul>
-            {notes.map(note => <Note key={note.id} note={note} toggleImportance={toggleImportance}/>)}
+            {notes && notes.map(note => <Note key={note.id} note={note} toggleImportance={toggleImportance}/>)}
         </ul>
     )
 }
 
 const Note = ({note,toggleImportance}) => {
     const label = note.important ? "make not important":"make important"
+    const inline_style = {
+        color: 'blue',
+        backgroundColor:'black',
+        textAlign:'left'
+    }
     
     return (
         <li className="note-item">
             {note.content}
-            <button className="importance" onClick={() => toggleImportance(note)}>{label}</button>
+            <button className="importance"  style={inline_style} onClick={() => toggleImportance(note)}>{label}</button>
         </li>
     )
 }
--- part2/courseinfo/website/src/services/communication.js
+++ part2/courseinfo/website/src/services/communication.js
@@ -9,15 +9,26 @@ const getAll = () => {
         date: '2019-05-30T17:30:31.098Z',
         important: true,
       }
-    return axios.get(baseUrl).then(response => response.data.concat(nonExisting))
+    return axios
+                .get(baseUrl)
+                .then(response => response.data.concat(nonExisting))
+                .catch(erorr => console.log(erorr))
+                .finally(() => console.log('getAll request'))
 }
 
 const update = (id, changedNote) =>  {
-    return axios.put(`${baseUrl}/${id}`,changedNote).then(response => response.data)
+    return axios
+                .put(`${baseUrl}/${id}`,changedNote)
+                .then(response => response.data)
+                .catch(error => console.log(error))
+                .finally(() => console.log('update request'))
 }
 
 const create = (newObject) => {
-    return axios.post(baseUrl,newObject)
+    return axios
+            .post(baseUrl,newObject)
+            .catch(erorr => console.log(erorr))
+            .finally(() => console.log('create request'))
 }
 
 export default {getAll, update, create}
