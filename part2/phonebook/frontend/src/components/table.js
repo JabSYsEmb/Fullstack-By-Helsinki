e3866c17c8e7d4416325451b4f128b58e6216d8a
JabSysEmb <abduljabbar.sha97@gmail.com>
2021-12-28 23:06:44 +0300

    exercise 2.17

--- part2/phonebook/frontend/src/components/table.js
+++ part2/phonebook/frontend/src/components/table.js
@@ -5,46 +5,33 @@ const TableHtml = ({phonebook}) => <Table phonebook={phonebook} />
 const Table = ({phonebook}) => 
 (
     <div className="phonebook">
-    <h2>Numbers</h2>
-    <table>
-        <TableHead simple={phonebook[0]}/>
-        <TableBody phonebook={phonebook} />
-    </table>
-</div>
+        <h2>Numbers</h2>
+        <table>
+            <TableHead simple={phonebook[0]}/>
+            <TableBody phonebook={phonebook} />
+        </table>
+    </div>
 )
+
+const TableHead = ({simple}) => <thead><tr>{simple && <GetNamesOfInstanceAsThArray simple={simple} />}</tr></thead>
 
 const GetNamesOfInstanceAsThArray = ({simple}) => Object.keys(simple).map(entry=> <th key={entry}>{entry}</th>).concat(<th key="delete">delete</th>)
 
-const TableHead = ({simple}) => 
-{
-    return (
-        <thead>
-            <tr>
-                {simple && <GetNamesOfInstanceAsThArray simple={simple} />}
-            </tr>
-        </thead>
-    )
+const TableBody = ({phonebook}) =>  phonebook?.map(contact => <TableLine key={contact.id} contact={contact}/>)
+
+const TableLine = ({contact}) => <tbody><TableContent contact={contact}/></tbody>
+
+const TableContent = ({contact}) => <tr><ArrayOfTd contact={contact} /></tr>
+
+const ArrayOfTd = ({contact}) => Object.values(contact).map(entry => <td key={entry}>{entry}</td>).concat(<td key="delete-btn"><DeleteBtn contact={contact}/></td>)
+
+const DeleteBtn = ({contact}) => <button type='submit' onClick={()=>deleteContactAndRefreshPage(contact)}>delete</button> 
+
+const deleteContactAndRefreshPage = (contact) => {
+    const {name} = contact
+    const msg = `Delete "${name}" from the phonebook?`
+    services.deleteByID(contact.id)
+    window.confirm(msg)
+    window.location.reload()
 }
-
-const TableBody = ({phonebook}) => 
-{
-    return phonebook?.map(contact => <TableLine key={contact.id} contact={contact}/>)
-}
-const TableLine = ({contact}) => (
-    <tbody>
-        <TableContent contact={contact}/>
-    </tbody>
-)
-
-const ArrayOfTd = ({contact}) => Object.values(contact).map(entry => <td key={entry}>{entry}</td>)
-
-const TableContent = ({contact}) => (
-    <tr>
-        <ArrayOfTd contact={contact} />
-        <td><DeleteBtn contact={contact}/></td>
-    </tr>
-)
-
-const DeleteBtn = ({contact}) => <button>delete</button> 
-
-export default TableHtml;  
+export default TableHtml;
