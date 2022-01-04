0b8bd3dfbc49b0d22619e43d3e83169ecc1e8ca9
JabSysEmb <abduljabbar.sha97@gmail.com>
2022-01-04 22:32:53 +0300

    refactor Country tag

--- part2/countries/src/components/CountryHtml.js
+++ part2/countries/src/components/CountryHtml.js
@@ -7,43 +7,25 @@ const CountryName = ({country}) => <p>{country.name.common}</p>
 
 const CountryHtml = ({country}) => country ? <Country country={country}/>:<NoDataHtml/>
 
-const Country = ({country}) => {
-return (
-<>
-  <CountryHeader country={country} />
-  <CountryInfo country={country}/>
-  <CountrySpokenLangauge country={country} />
-  <CountryFlag country={country}/>
-</>)
-}
-
-const CountryHeader = ({country}) => <h2>{country.name.common}</h2>
-const CountryInfo   = ({country}) => {
-  return (
-  <div className="country-info" >
-    <p>Capital : {country.capital[0]}</p>
-    <p>population : {country.population}</p>
-  </div>
-  )
-}
-
-const CountrySpokenLangauge = ({country}) => {
-  return (
-    <>
+const Country = ({ country }) => (
+  <>
+    <h2>{country.name.common}</h2>
+    <div className="country-info">
+      <p>Capital: {country.capital[0]}</p>
+      <p>Population: {country.population}</p>
       <h2>Languages</h2>
       <ul>
-        {< Langauges languages={country.languages}/>}
+        {Object.entries(country.languages).map(([key, language]) => (
+          <li key={key}>{language}</li>
+        ))}
       </ul>
-    </>
-  )
-}
-const Langauges = ({languages}) => {
-  const langs = [];
-  for (const lang in languages) {
-    langs.push(<li key={lang}>{languages[lang]}</li>)
-  }
-  return langs;
-}
-const CountryFlag = ({country}) => <img className="flag-class" src={country.flags.svg} alt="country-flag"/>
+      <img
+        className="flag-class"
+        src={country.flags.svg}
+        alt={country.name.common}
+      />
+    </div>
+  </>
+);
 
 export {CountryHtml,MatchesCountryHtml};
