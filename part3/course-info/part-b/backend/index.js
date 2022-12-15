25defbe648c563050e58a5cb880a5f5e6ea28187
JabSysEmb <abduljabbar.sha97@gmail.com>
2022-12-15 12:06:44 +0300

    work on parser

--- part3/course-info/part-b/backend/index.js
+++ part3/course-info/part-b/backend/index.js
@@ -13,4 +13,6 @@ app.get("/api/notes", (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT => {console.log(`The server is listening on the ${PORT}`)});
+app.listen(PORT,() => {
+  console.log(`The server is listening on the ${PORT}`)
+});
--- part3/course-info/part-b/frontend/src/App.js
+++ part3/course-info/part-b/frontend/src/App.js
@@ -2,25 +2,34 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
+const {REACT_APP_BACKEND, ...rest} = process.env;
 
-console.log(process.env);
-
-const {REACT_APP_BACKEND} = process.env;
-
+console.log(rest)
 
 function App() {
   const [data, setData] = useState(()=>{});
+  const [msg,  setMsg]  = useState(()=>{});
   
   useEffect(() => {
     axios.get(`${REACT_APP_BACKEND}/api/notes`).then(res => {
       setData(res.data);
     });
   },[]);
+  
+  const btnClickMeHandler = () => {
+    setMsg(() => "fuck you")
+  };
+
+  useEffect(() => () => setTimeout(btnClickMeHandler, 2000),[]);
 
   return (
-    <div className="App">
-      {data && data.map(note => <p className="note" key={note.id}> {note.content} </p>)}
-    </div>
+    <>
+      <h1 style={{visibility: msg ? 1:0}}>{msg}</h1>
+      <div className="App">
+        {data && data.map(note => <p className="note" key={note.id}> {note.content} </p>)}
+      </div>
+      <button onClick={btnClickMeHandler}>Click Me</button>
+    </>
   );
 }
 
